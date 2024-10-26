dc235050fe5720f582e7ab05192fa1fc65070a02
Matt-Gab <matthewe.egabriel@gmail.com>
2024-10-26 20:21:12 +0800

    trying new way to compare

--- show_the_oldest_inputted_person.py
+++ show_the_oldest_inputted_person.py
@@ -4,12 +4,24 @@ age = []
 
 while True:
     while True:
-        while True:                                                     # loop2 input and check if error
+        while True:                                             # loop2 input and check if error
             try:
                 name.append(input('Input your username: '))
                 age.append(int(input('Input your age: ')))
-                if name.isalpha() and age > 0 and age < 123:
-                    if age[0] > age[1]:
+                
+                if name.isalpha() and age > 0 and age < 123:    # statements check if correct or bigger than the last
+                    if name[1] in name and age[1] in age:
+                        if age[0] > age[1]:
+                            oldest_user = dict(zip(name, age))
+                            name.remove(name[ : -2])
+                            age.remove(age[ : -2])
+                        if age[0] == age[1]:
+                            oldest_user = dict(zip(name, age))
+                        if age[0] < age[1]:
+                            oldest_user = dict(zip(name, age))
+                            name.remove(name[ : -1])
+                            age.remove(age[ : -1]) 
+                    else:
                         oldest_user = dict(zip(name, age))
                     break
                 else:
@@ -24,6 +36,6 @@ while True:
         break
     
     if input_again == "N" or 'n':
-        print('hello / placeholder')
+        print(oldest_user)
     elif input_again != 'Y' or 'y':
         print('Invalid input')
