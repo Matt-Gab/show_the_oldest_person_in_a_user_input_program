21817663a905b95294841157b5507030eb0626a1
Matt-Gab <matthewe.egabriel@gmail.com>
2024-10-26 22:24:01 +0800

    promising update, removed dictionary

--- show_the_oldest_inputted_person.py
+++ show_the_oldest_inputted_person.py
@@ -1,30 +1,27 @@
 name = []
 age = []
-oldest_user = {}                                                # purpose: show the name of oldest
-                                                                # bugs: cant save for multiple same age
+                                                                # purpose: show the name of oldest
+                                                                # known bugs: cant save for multiple same age and name
 while True:                                                     # loop1 procs the program to end+show oldest or redo
     while True:                                                 # loop2 asks for re input
         while True:                                             # loop3 asks for input and check error and invalid
             try:
-                name.append(input('Input your name: '))
-                age.append(int(input('Input your age: ')))
+                name.append(input('Input your name. Alphabet only\n'))
+                age.append(int(input('Input your age. Integers only\n')))
             except:
                 print('Your inputted data has caused an error')
             
-            if name[0].isalpha() == True and age[0] > 0 == True:       # check if data is valid
+            if name[0].isalpha() == True and age[0] > 0 and age[0] < 123:       # check if data is valid
                 try:
-                    if name[1] in name and age[1] in age:                                           # asks if 2nd index exist
-                        if age[0] > age[1]:                                                         # if 2nd index exist, compares if bigger or not
-                            name.pop(-2)                                                            # put in dic if bigger
+                    if name[1] in name and age[1] in age:                       # asks if 2nd index exist
+                        if age[0] > age[-1]:                                     # if 2nd index exist, compares if bigger or not
+                            name.pop(-1)                                        # put in dic if bigger
+                            age.pop(-1)
+                        if age[0] < age[-1]:
+                            name.pop(-2)
                             age.pop(-2)
-                        if age[0] == age[1]:
-                            oldest_user = dict(zip(name, age))
-                        if age[0] < age[1]:
-                            oldest_user = dict(zip(name, age))
-                            name.pop(-1)
-                            age.pop(-1) 
                 except:
-                    oldest_user = dict(zip(name, age))
+                    break                                       # break loop3 too
                 break                                           # break loop3
             else:
                 print('\nERROR. Inputted data is removed. Please input valid data.\n')
@@ -34,8 +31,10 @@ while True:                                                     # loop1 procs th
         input_again = input('Do you want to input another user? Y/N\n')
         break                                                   # break loop2
     
-    if input_again == "N" or 'n':
-        print(oldest_user)
+    if input_again == "N":
+        print('The oldest are')
+        for i in range(len(name)):
+            print(name[i], age[i])
         break                                                   # break loop1
-    elif input_again != 'Y' or 'y':
+    elif input_again != 'Y':
         print('Invalid input')
